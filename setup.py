a1ee333de32a69814fe05ae7e124f81cbc061661
Rian Bogley <rianbogley@gmail.com>
2023-04-19 14:55:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ALBA_Pipeline',
+    name='rian-alba-tools',
     version='0.1',
     packages=find_packages(),
     install_requires=[
